ffaddff3bbb5ec516874bdf59b3536971d6aa8c2
yoshiki9636 <61720120+yoshiki9636@users.noreply.github.com>
2023-12-15 23:43:29 +0900

    change if

--- sim/sys4_simtop1.v
+++ sim/sys4_simtop1.v
@@ -13,20 +13,32 @@ module simtop;
 reg clkin;
 reg rst_n;
 
-reg ren; // input
+reg dma_io_we; // input
+reg [15:2] dma_io_wadr; // input
+reg [15:0] dma_io_wdata; // input
+reg [15:2] dma_io_radr; // input
+wire [15:0] dma_io_rdata_in = 16'd0; // input
+wire [15:0] dma_io_rdata; // output
+reg ibus_ren; // input
 reg [15:0] ibus_radr; // input
 wire [15:0] ibus_rdata; // output
-reg wen; // input
+reg ibus_wen; // input
 reg [15:0] ibus_wadr; // input
 reg [15:0] ibus_wdata; // input
 
 systolic4 systolic4 (
 	.clk(clkin),
 	.rst_n(rst_n),
-	.ren(ren),
+	.dma_io_we(dma_io_we),
+	.dma_io_wadr(dma_io_wadr),
+	.dma_io_wdata(dma_io_wdata),
+	.dma_io_radr(dma_io_radr),
+	.dma_io_rdata_in(dma_io_rdata_in),
+	.dma_io_rdata(dma_io_rdata),
+	.ibus_ren(ibus_ren),
 	.ibus_radr(ibus_radr),
 	.ibus_rdata(ibus_rdata),
-	.wen(wen),
+	.ibus_wen(ibus_wen),
 	.ibus_wadr(ibus_wadr),
 	.ibus_wdata(ibus_wdata)
 	);
@@ -48,42 +60,44 @@ initial $readmemh("./testb1.txt", systolic4.iobuf.b1buf.buf_ab.ram);
 `define OBUFS1_0_HEAD 7'h41
 `define OBUFS0_1_HEAD 7'h42
 `define OBUFS1_1_HEAD 7'h43
-`define SYS_START_ADR 16'hFFF0
-`define SYS_MAX_CNTR 16'hFFF1
-`define SYS_RUN_CNTR 16'hFFF2
+`define SYS_START_ADR 14'h3FF8
+`define SYS_MAX_CNTR 14'h3FF9
+`define SYS_RUN_CNTR 14'h3FFa
 
 initial begin
-	ren = 1'b0;
+	ibus_ren = 1'b0;
 	ibus_radr = 16'd0;
-	wen = 1'b0;
+	ibus_wen = 1'b0;
 	ibus_wadr = 16'd0;
 	ibus_wdata = 16'd0;
+	dma_io_we = 1'b0;
+	dma_io_wadr = 14'd0;
+	dma_io_radr = 14'd0;
+	dma_io_wdata = 16'd0;
 	rst_n = 1'b1;
 #10
 	rst_n = 1'b0;
 #20
 	rst_n = 1'b1;
 #10
-	ibus_wadr = `SYS_MAX_CNTR;
-	ibus_wdata = 16'd3;
-	wen = 1'b1;
+	dma_io_wadr = `SYS_MAX_CNTR;
+	dma_io_wdata = 16'd3;
+	dma_io_we = 1'b1;
 #10
-	ibus_wadr = `SYS_RUN_CNTR;
-	ibus_wdata = 16'd3;
+	dma_io_wadr = `SYS_RUN_CNTR;
+	dma_io_wdata = 16'd3;
 #10
-	wen = 1'b0;
+	dma_io_we = 1'b0;
 #10
-	ibus_wadr = `SYS_START_ADR;
-	ibus_wdata = 16'hffff;
-	wen = 1'b1;
+	dma_io_wadr = `SYS_START_ADR;
+	dma_io_wdata = 16'hffff;
+	dma_io_we = 1'b1;
 #10
-	wen = 1'b0;
-	ren = 1'b1;
+	dma_io_we = 1'b0;
 #20
-	ibus_radr = `SYS_START_ADR;
+	dma_io_radr = `SYS_START_ADR;
 #2000
-	ibus_radr = `SYS_START_ADR;
-#10
+	ibus_ren = 1'b1;
 	ibus_radr = { `OBUFS0_0_HEAD, 9'h000 };
 #10
 	ibus_radr = { `OBUFS0_0_HEAD, 9'h001 };
@@ -160,92 +174,95 @@ initial begin
 #10
 	ibus_radr = { `OBUFS1_1_HEAD, 9'h100 };
 #100
-	ren = 1'b0;
-	ibus_wadr = { `IBUFA0_HEAD, 8'h00 };
+	ibus_ren = 1'b0;
+	ibus_wen = 1'b1;
+	ibus_wadr = { `IBUFA0_HEAD, 10'h000 };
 	ibus_wdata = 16'h0000;
 #10
-	ibus_wadr = { `IBUFA0_HEAD, 8'h01 };
+	ibus_wadr = { `IBUFA0_HEAD, 10'h001 };
 	ibus_wdata = 16'h1111;
 #10
-	ibus_wadr = { `IBUFA0_HEAD, 8'h02 };
+	ibus_wadr = { `IBUFA0_HEAD, 10'h002 };
 	ibus_wdata = 16'h2222;
 #10
-	ibus_wadr = { `IBUFA0_HEAD, 8'h03 };
+	ibus_wadr = { `IBUFA0_HEAD, 10'h003 };
 	ibus_wdata = 16'h3333;
 #10
 
-	ibus_wadr = { `IBUFA1_HEAD, 8'h04 };
+	ibus_wadr = { `IBUFA1_HEAD, 10'h004 };
 	ibus_wdata = 16'h4444;
 #10
-	ibus_wadr = { `IBUFA1_HEAD, 8'h05 };
+	ibus_wadr = { `IBUFA1_HEAD, 10'h005 };
 	ibus_wdata = 16'h5555;
 #10
-	ibus_wadr = { `IBUFA1_HEAD, 8'h06 };
+	ibus_wadr = { `IBUFA1_HEAD, 10'h006 };
 	ibus_wdata = 16'h6666;
 #10
-	ibus_wadr = { `IBUFA1_HEAD, 8'h07 };
+	ibus_wadr = { `IBUFA1_HEAD, 10'h007 };
 	ibus_wdata = 16'h7777;
 #10
 
-	ibus_wadr = { `IBUFB0_HEAD, 8'h00 };
+	ibus_wadr = { `IBUFB0_HEAD, 10'h000 };
 	ibus_wdata = 16'h8888;
 #10
-	ibus_wadr = { `IBUFB0_HEAD, 8'h01 };
+	ibus_wadr = { `IBUFB0_HEAD, 10'h001 };
 	ibus_wdata = 16'h9999;
 #10
-	ibus_wadr = { `IBUFB0_HEAD, 8'h02 };
+	ibus_wadr = { `IBUFB0_HEAD, 10'h002 };
 	ibus_wdata = 16'haaaa;
 #10
-	ibus_wadr = { `IBUFB0_HEAD, 8'h03 };
+	ibus_wadr = { `IBUFB0_HEAD, 10'h003 };
 	ibus_wdata = 16'hbbbb;
 
 #10
-	ibus_wadr = { `IBUFB1_HEAD, 8'h04 };
+	ibus_wadr = { `IBUFB1_HEAD, 10'h004 };
 	ibus_wdata = 16'hcccc;
 #10
-	ibus_wadr = { `IBUFB1_HEAD, 8'h05 };
+	ibus_wadr = { `IBUFB1_HEAD, 10'h005 };
 	ibus_wdata = 16'hdddd;
 #10
-	ibus_wadr = { `IBUFB1_HEAD, 8'h06 };
+	ibus_wadr = { `IBUFB1_HEAD, 10'h006 };
 	ibus_wdata = 16'heeee;
 #10
-	ibus_wadr = { `IBUFB1_HEAD, 8'h07 };
+	ibus_wadr = { `IBUFB1_HEAD, 10'h007 };
 	ibus_wdata = 16'hffff;
+#10
+	ibus_wen = 1'b0;
 
 #100
-	ren = 1'b1;
-	ibus_radr = { `IBUFA0_HEAD, 8'h00 };
+	ibus_ren = 1'b1;
+	ibus_radr = { `IBUFA0_HEAD, 10'h000 };
 #10
-	ibus_radr = { `IBUFA0_HEAD, 8'h01 };
+	ibus_radr = { `IBUFA0_HEAD, 10'h001 };
 #10
-	ibus_radr = { `IBUFA0_HEAD, 8'h02 };
+	ibus_radr = { `IBUFA0_HEAD, 10'h002 };
 #10
-	ibus_radr = { `IBUFA0_HEAD, 8'h03 };
+	ibus_radr = { `IBUFA0_HEAD, 10'h003 };
 #10
-	ibus_radr = { `IBUFA1_HEAD, 8'h04 };
+	ibus_radr = { `IBUFA1_HEAD, 10'h004 };
 #10
-	ibus_radr = { `IBUFA1_HEAD, 8'h05 };
+	ibus_radr = { `IBUFA1_HEAD, 10'h005 };
 #10
-	ibus_radr = { `IBUFA1_HEAD, 8'h06 };
+	ibus_radr = { `IBUFA1_HEAD, 10'h006 };
 #10
-	ibus_radr = { `IBUFA1_HEAD, 8'h07 };
+	ibus_radr = { `IBUFA1_HEAD, 10'h007 };
 #10
 
-	ibus_radr = { `IBUFB0_HEAD, 8'h00 };
+	ibus_radr = { `IBUFB0_HEAD, 10'h000 };
 #10
-	ibus_radr = { `IBUFB0_HEAD, 8'h01 };
+	ibus_radr = { `IBUFB0_HEAD, 10'h001 };
 #10
-	ibus_radr = { `IBUFB0_HEAD, 8'h02 };
+	ibus_radr = { `IBUFB0_HEAD, 10'h002 };
 #10
-	ibus_radr = { `IBUFB0_HEAD, 8'h03 };
+	ibus_radr = { `IBUFB0_HEAD, 10'h003 };
 #10
-	ibus_radr = { `IBUFB1_HEAD, 8'h04 };
+	ibus_radr = { `IBUFB1_HEAD, 10'h004 };
 #10
-	ibus_radr = { `IBUFB1_HEAD, 8'h05 };
+	ibus_radr = { `IBUFB1_HEAD, 10'h005 };
 #10
-	ibus_radr = { `IBUFB1_HEAD, 8'h06 };
+	ibus_radr = { `IBUFB1_HEAD, 10'h006 };
 #10
-	ibus_radr = { `IBUFB1_HEAD, 8'h07 };
+	ibus_radr = { `IBUFB1_HEAD, 10'h007 };
 #1000
 
 	$stop;
