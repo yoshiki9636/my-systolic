6c1ae77ac6586b6ae33b93f237f34a464b373ef5
yoshiki9636 <61720120+yoshiki9636@users.noreply.github.com>
2023-12-22 22:09:39 +0900

    fixed bug related parameter

--- tools/matrix_file_gen.py
+++ tools/matrix_file_gen.py
@@ -45,7 +45,7 @@ mem_interval = matsize * itter_size * itter_size
 dc = mem_interval - 1
 print("#immdefine imm_dma_dc 0x{0:03x}".format(dc))
 mem_start = 0
-for i in range(4):
+for i in range(2*syssize):
 	print("#immdefine imm_mem_start{0} 0x{1:03x}".format(i,mem_start))
 	mem_start += mem_interval * 4
 
@@ -66,11 +66,11 @@ dc_s_2 = int(dc_s / 16)
 dc_s_2 += 1 if (dc_s_2 == 0) else 0
 print("#immdefine imm_dma_dc_s_2 0x{0:03x}".format(dc_s_2))
 
-for i in range(4):
+for i in range(2*syssize):
 	print("#immdefine imm_mem_start_s{0} 0x{1:03x}".format(i,mem_start))
 	mem_start += dc_s * 4
 
-for i in range(4):
+for i in range(2*syssize):
 	print("#immdefine imm_mem_start_s{0}s 0x{1:03x}".format(i,mem_start))
 	mem_start += 8 if dc_s_2 == 1 else dc_s_2
 
